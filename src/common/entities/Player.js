8d4aef4ddee4a3dd6eca3f730c34a91c6be99816
Daniel <choiwaim@gmail.com>
2021-03-02 18:15:37 +1100

    added player username

--- src/common/entities/Player.js
+++ src/common/entities/Player.js
@@ -1,5 +1,33 @@
 import { getScene, setPlayer } from "../../client/Model/state.js";
-import { SceneLoader, Vector3 } from "@babylonjs/core";
+import { SceneLoader, Vector3, MeshBuilder } from "@babylonjs/core";
+import {
+  AdvancedDynamicTexture,
+  Rectangle,
+  TextBlock,
+} from "@babylonjs/gui";
+
+const showUsername = (mesh, username) => {
+  var container = new Rectangle();
+  container.height = 0.15;
+  container.width = 0.6;
+  container.cornerRadius = 20;
+  container.color = "white";
+  container.thickness = 0;
+  container.background = "#00000066";
+
+  var plane = MeshBuilder.CreatePlane("plane", {}, getScene());
+  plane.parent = mesh;
+  plane.position.y = 1;
+
+  var advancedTexture = AdvancedDynamicTexture.CreateForMesh(plane);
+
+  var label = new TextBlock();
+  label.text = username ?? "Guest";
+  label.fontSize = "68px";
+  container.addControl(label);
+
+  advancedTexture.addControl(container);
+};
 
 export default class Player {
   constructor(player) {
@@ -14,12 +42,14 @@ export default class Player {
 
     SceneLoader.ImportMeshAsync(
       null,
-      `./graphics/character/${character === "male-player" ? "boy" : "girl"}.glb`,
+      `./graphics/character/${
+        character === "male-player" ? "boy" : "girl"
+      }.glb`,
       null,
       scene
     ).then(({ meshes, animationGroups }) => {
       let player = meshes[0];
-
+      showUsername(player, this.name);
       player.position = new Vector3(x, y, z);
       player.ellipsoidOffset = new Vector3(0, 1, 0);
       player.checkCollisions = true;
